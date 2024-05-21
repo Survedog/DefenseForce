0a6d4241d356ab54de6bb43a5dd22416c04ac091
survedog <survedog@gmail.com>
2024-05-21 12:36:05 +0900

    [FEAT]DFPlayerController에 타워 조종 시작/중단 함수 구현

--- Source/DefenseForce/Player/DFPlayerController.cpp
+++ Source/DefenseForce/Player/DFPlayerController.cpp
@@ -2,4 +2,43 @@
 
 
 #include "Player/DFPlayerController.h"
+#include "Player/DFPlayerPawn.h"
+#include "AbilitySystemComponent.h"
+#include "Structure/DFTowerBase.h"
 
+ADFPlayerController::ADFPlayerController() : DFPlayerPawn(nullptr), CurrentControlledTower(nullptr)
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
+}
+
+void ADFPlayerController::StartTowerControl(ADFTowerBase* NewTower)
+{
+	if (!NewTower->IsBeingControlled())
+	{
+		if (CurrentControlledTower.IsValid())
+		{
+			EndTowerControl();
+		}
+		CurrentControlledTower = NewTower;
+		CurrentControlledTower->BeginControl(DFPlayerPawn);
+	}
+}
+
+void ADFPlayerController::EndTowerControl()
+{
+	if (CurrentControlledTower.IsValid() && CurrentControlledTower->IsBeingControlled())
+	{
+		CurrentControlledTower->EndControl();
+		CurrentControlledTower = nullptr;
+	}
+}
+
+void ADFPlayerController::OnPossess(APawn* aPawn)
+{
+	Super::OnPossess(aPawn);
+
+	DFPlayerPawn = CastChecked<ADFPlayerPawn>(aPawn);
+}
--- Source/DefenseForce/Player/DFPlayerController.h
+++ Source/DefenseForce/Player/DFPlayerController.h
@@ -14,4 +14,22 @@ class DEFENSEFORCE_API ADFPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	ADFPlayerController();
+
+	UFUNCTION(BlueprintCallable)
+	void StartTowerControl(class ADFTowerBase* NewTower);
+
+	UFUNCTION(BlueprintCallable)
+	void EndTowerControl();
+
+protected:
+	virtual void OnPossess(APawn* aPawn) override;
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Player")
+	TObjectPtr<class ADFPlayerPawn> DFPlayerPawn;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Control")
+	TWeakObjectPtr<class ADFTowerBase> CurrentControlledTower;
 };
