8c00c79d6b9915c1c0404a7946cbf1b26c29540a
survedog <survedog@gmail.com>
2024-05-21 09:35:40 +0900

    [FIX]PublicIncludePath DefenseForce 추가

--- Source/DefenseForce/DefenseForce.Build.cs
+++ Source/DefenseForce/DefenseForce.Build.cs
@@ -7,8 +7,10 @@ public class DefenseForce : ModuleRules
 	public DefenseForce(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+
+        PublicIncludePaths.AddRange(new string[] { "DefenseForce" });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
