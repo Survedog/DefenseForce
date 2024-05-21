573d31899e2898507a181db8f5b8355325a9285f
survedog <survedog@gmail.com>
2024-05-21 10:13:52 +0900

    [FEAT]DFPlayerState에 ASC 프로퍼티 추가

--- Source/DefenseForce/Player/DFPlayerState.cpp
+++ Source/DefenseForce/Player/DFPlayerState.cpp
@@ -2,4 +2,18 @@
 
 
 #include "Player/DFPlayerState.h"
+#include "AbilitySystemComponent.h"
 
+ADFPlayerState::ADFPlayerState()
+{
+	ASC = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("ASC"));
+	ASC->SetIsReplicated(true);
+	ASC->ReplicationMode = EGameplayEffectReplicationMode::Mixed;
+
+	NetUpdateFrequency = 50.0f;
+}
+
+UAbilitySystemComponent* ADFPlayerState::GetAbilitySystemComponent() const
+{
+	return ASC;
+}
--- Source/DefenseForce/Player/DFPlayerState.h
+++ Source/DefenseForce/Player/DFPlayerState.h
@@ -4,14 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
 #include "DFPlayerState.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class DEFENSEFORCE_API ADFPlayerState : public APlayerState
+class DEFENSEFORCE_API ADFPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 	
+public:
+	ADFPlayerState();
+
+	virtual class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = GAS)
+	TObjectPtr<class UAbilitySystemComponent> ASC;
 };
