1b29d71ae8eda01c77839b968c119611b69959ab
survedog <survedog@gmail.com>
2024-05-21 11:41:52 +0900

    [FEAT]DFTowerBase에조종 시작/해제 기능 추가

--- Source/DefenseForce/Structure/DFTowerBase.cpp
+++ Source/DefenseForce/Structure/DFTowerBase.cpp
@@ -2,8 +2,36 @@
 
 
 #include "Structure/DFTowerBase.h"
+#include "Player/DFPlayerPawn.h"
+#include "Net/UnrealNetwork.h"
 
-ADFTowerBase::ADFTowerBase()
+ADFTowerBase::ADFTowerBase() : ControllingPlayerPawn(nullptr)
 {
 	PrimaryActorTick.bCanEverTick = true;
+	
+	bIsBeingControlled = false;
+}
+
+void ADFTowerBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ADFTowerBase, ControllingPlayerPawn);
+}
+
+void ADFTowerBase::BeginControl_Implementation(ADFPlayerPawn* NewControllingPlayer)
+{
+	ControllingPlayerPawn = NewControllingPlayer;
+	bIsBeingControlled = true;
+}
+
+void ADFTowerBase::EndControl_Implementation()
+{
+	ControllingPlayerPawn = nullptr;
+	bIsBeingControlled = false;
+}
+
+void ADFTowerBase::OnRep_ControllingPlayerPawn_Implementation()
+{
+	bIsBeingControlled = ControllingPlayerPawn != nullptr ? true : false;
 }
--- Source/DefenseForce/Structure/DFTowerBase.h
+++ Source/DefenseForce/Structure/DFTowerBase.h
@@ -16,4 +16,27 @@ class DEFENSEFORCE_API ADFTowerBase : public ADFStructureBase
 	
 public:
 	ADFTowerBase();
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE bool IsBeingControlled() { return bIsBeingControlled; }
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void BeginControl(class ADFPlayerPawn* NewControllingPlayer);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void EndControl();
+
+protected:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnRep_ControllingPlayerPawn();
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, ReplicatedUsing = OnRep_ControllingPlayerPawn, Category = "Player Control")
+	TWeakObjectPtr<class ADFPlayerPawn> ControllingPlayerPawn;
+
+private:
+	UPROPERTY(VisibleInstanceOnly)
+	uint8 bIsBeingControlled : 1;
 };
