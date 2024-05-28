3b950640f560bf5cab2e2ae27525bc2f0aad6b84
survedog <survedog@gmail.com>
2024-05-29 00:59:17 +0900

    [FEAT]DFGATA_Trace: 마우스 위치를 향해 Trace하는 TargetActor로서 구현. [FEAT]DFGATA_Trace: 재사용을 위해 Confirm/Cancel 시 Destroy하지 않도록 재정의. [FIX]GA_BuildStructure: ActivateAbility 시 타겟 데이터를 기다리는 태스크 생성 및 콜백 함수 등록. 각 함수에 로깅 함수 추가.

--- Source/DefenseForce/GAS/GA/GA_BuildStructure.cpp
+++ Source/DefenseForce/GAS/GA/GA_BuildStructure.cpp
@@ -2,12 +2,56 @@
 
 
 #include "GAS/GA/GA_BuildStructure.h"
+#include "Abilities/Tasks/AbilityTask_WaitTargetData.h"
+#include "GAS/TA/DFGATA_Trace.h"
 #include "DFLog.h"
 
-void UGA_BuildStructure::InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+//void UGA_BuildStructure::InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+//{
+//	// Don't call original super function.
+//	bool bReplicateEndAbility = true;
+//	bool bWasCancelled = false;
+//	EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+//}
+
+void UGA_BuildStructure::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	DF_NETGASLOG(LogDFGAS, Log, TEXT("Start"));
+	TSubclassOf<AGameplayAbilityTargetActor> TargetActorClass = ADFGATA_Trace::StaticClass();
+	UAbilityTask_WaitTargetData* WaitTargetDataTask = UAbilityTask_WaitTargetData::WaitTargetData(this, FName("WaitBuildTargetData"), EGameplayTargetingConfirmation::Type::UserConfirmed, TargetActorClass);
+	if (WaitTargetDataTask)
+	{
+		AGameplayAbilityTargetActor* TargetActor = nullptr;
+		WaitTargetDataTask->ValidData.AddDynamic(this, &UGA_BuildStructure::OnTargetDataReadyCallback);
+		WaitTargetDataTask->Cancelled.AddDynamic(this, &UGA_BuildStructure::OnTargetDataCancelledCallback);
+		if (WaitTargetDataTask->BeginSpawningActor(this, TargetActorClass, TargetActor))
+		{
+			WaitTargetDataTask->FinishSpawningActor(this, TargetActor);
+		}
+	}
+}
+
+void UGA_BuildStructure::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	DF_NETGASLOG(LogDFGAS, Log, TEXT("Start"));
+}
+
+void UGA_BuildStructure::InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
 {
 	// Don't call original super function.
-	bool bReplicateEndAbility = true;
-	bool bWasCancelled = false;
-	EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+	DF_NETGASLOG(LogDFGAS, Log, TEXT("Start"));
+}
+
+void UGA_BuildStructure::OnTargetDataReadyCallback(const FGameplayAbilityTargetDataHandle& TargetDataHandle)
+{
+	DF_NETGASLOG(LogDFGAS, Log, TEXT("Start"));
+}
+
+void UGA_BuildStructure::OnTargetDataCancelledCallback(const FGameplayAbilityTargetDataHandle& TargetDataHandle)
+{
+	DF_NETGASLOG(LogDFGAS, Log, TEXT("Start"));
 }
--- Source/DefenseForce/GAS/GA/GA_BuildStructure.h
+++ Source/DefenseForce/GAS/GA/GA_BuildStructure.h
@@ -16,9 +16,15 @@ class DEFENSEFORCE_API UGA_BuildStructure : public UGameplayAbility
 	
 protected:
 	//virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
-	//virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
-	//virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
-	//virtual void InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
-	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+	virtual void InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+	//virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+
+	UFUNCTION()
+	void OnTargetDataReadyCallback(const FGameplayAbilityTargetDataHandle& TargetDataHandle);
+
+	UFUNCTION()
+	void OnTargetDataCancelledCallback(const FGameplayAbilityTargetDataHandle& TargetDataHandle);
 };
