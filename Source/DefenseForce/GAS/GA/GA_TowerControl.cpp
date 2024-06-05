30a6d5c3bf559f4ae1b369e0232ac60341ab4177
survedog <survedog@gmail.com>
2024-06-05 19:13:47 +0900

    [FEAT]GA_TowerControl: 타겟 액터를 통해 조준하는 로직 구현

--- Source/DefenseForce/GAS/GA/GA_TowerControl.cpp
+++ Source/DefenseForce/GAS/GA/GA_TowerControl.cpp
@@ -2,10 +2,82 @@
 
 
 #include "GAS/GA/GA_TowerControl.h"
+#include "GAS/AT/DFAT_WaitTargetData_ReusableTA.h"
+#include "Subsystem/TargetingInstanceSubsystem.h"
+#include "GAS/TA/DFGATA_Trace.h"
+#include "AbilitySystemComponent.h"
+#include "Interface/PlayerTowerControlInterface.h"
+#include "Structure/DFTowerBase.h"
 #include "DFLog.h"
 
-void UGA_TowerControl::InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+void UGA_TowerControl::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
-	DF_NETGASLOG(LogDF, Log, TEXT("Start"));
-	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	DF_NETGASLOG(LogDFGAS, Log, TEXT("Start"));
+
+	IPlayerTowerControlInterface* TowerControlInterface = CastChecked<IPlayerTowerControlInterface>(ActorInfo->AvatarActor);
+	ADFTowerBase* TowerToControl = Cast<ADFTowerBase>(TowerControlInterface->GetCurrentStructureUnderCursor());
+	if (!TowerToControl)
+	{
+		K2_CancelAbility();
+	}
+
+	if (UTargetingInstanceSubsystem* TargetingInstanceSubsystem = ULocalPlayer::GetSubsystem<UTargetingInstanceSubsystem>(ActorInfo->PlayerController->GetLocalPlayer()))
+	{
+		if (DFTargetActor = TargetingInstanceSubsystem->GetTargetActorFromClass<ADFGATA_Trace>(*TowerToControl->GetTargetActorClass()))
+		{
+			DFTargetActor->TraceProfile = FCollisionProfileName(TEXT("BlockOnlyWorld"));
+			DFTargetActor->bIgnoreBlockingHits = false;
+			DFTargetActor->bTraceStartsFromPlayerCamera = true;
+			DFTargetActor->bTraceTowardMouseAimLocation = true;
+		}
+	}
+
+	if (DFTargetActor)
+	{		
+		DFTargetActor->SourceActor = TowerToControl;
+		DFTargetActor->ReticleClass = TowerToControl->GetReticleClass();
+		GetAbilitySystemComponentFromActorInfo()->SpawnedTargetActors.Push(DFTargetActor);
+
+		// TODO: Reuse AbilityTask
+		UDFAT_WaitTargetData_ReusableTA* WaitTargetDataTask = UDFAT_WaitTargetData_ReusableTA::WaitTargetDataUsingReusableTA(this, FName("WaitAttackTargetData"), EGameplayTargetingConfirmation::Type::CustomMulti, DFTargetActor);
+		if (WaitTargetDataTask)
+		{
+			WaitTargetDataTask->ValidData.AddDynamic(this, &UGA_TowerControl::OnTargetDataReadyCallback);
+			WaitTargetDataTask->ReadyForActivation();
+		}
+	}
+	else
+	{
+		DF_NETGASLOG(LogDFGAS, Error, TEXT("Target actor is nullptr."));
+		CancelAbility(Handle, ActorInfo, ActivationInfo, true);
+	}
+}
+
+void UGA_TowerControl::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	DF_NETGASLOG(LogDFGAS, Log, TEXT("Start. EndState: %s"), bWasCancelled ? TEXT("Cancelled") : TEXT("Confirmed"));
+	GetAbilitySystemComponentFromActorInfo()->SpawnedTargetActors.Remove(DFTargetActor);
+	DFTargetActor = nullptr;
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UGA_TowerControl::InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	DF_NETGASLOG(LogDFGAS, Log, TEXT("Start"));	
+	IPlayerTowerControlInterface* TowerControlInterface = CastChecked<IPlayerTowerControlInterface>(ActorInfo->AvatarActor);
+	ADFTowerBase* ControlledTower = TowerControlInterface->GetCurrentControlledTower();
+	if (ControlledTower && ControlledTower->ShouldConfirmTargetOnInputPressed())
+	{
+		DFTargetActor->ConfirmTargetingAndContinue();
+	}
+}
+
+void UGA_TowerControl::OnTargetDataReadyCallback_Implementation(const FGameplayAbilityTargetDataHandle& TargetDataHandle)
+{
+	DF_NETGASLOG(LogDFGAS, Log, TEXT("Start"));
+	FGameplayEventData Payload;
+	Payload.TargetData = TargetDataHandle;
+	GetAbilitySystemComponentFromActorInfo()->HandleGameplayEvent(FGameplayTag::RequestGameplayTag(TEXT("Structure.Action.Attack")), &Payload);
 }
--- Source/DefenseForce/GAS/GA/GA_TowerControl.h
+++ Source/DefenseForce/GAS/GA/GA_TowerControl.h
@@ -15,10 +15,15 @@ class DEFENSEFORCE_API UGA_TowerControl : public UGameplayAbility
 	GENERATED_BODY()
 	
 protected:
-	//virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
-	//virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
-	//virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
-	//virtual void InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
-	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+	virtual void InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnTargetDataReadyCallback(const FGameplayAbilityTargetDataHandle& TargetDataHandle);
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Targeting")
+	TObjectPtr<class ADFGATA_Trace> DFTargetActor;
 };
