486b9b99f9b2c84464650f985677878c664f7e65
survedog <survedog@gmail.com>
2024-06-10 19:35:09 +0900

    [FEAT]DFGameplayTags.h에 게임플레이 태그를 가져오는 매크로를 선언

---
+++ Source/DefenseForce/GAS/DFGameplayTags.h
@@ -0,0 +1,8 @@
+// Copyright 2024, Survedog. All rights reserved.
+
+#pragma once
+
+#include "GameplayTagContainer.h"
+
+#define GASTAG_Structure_Action_Attack FGameplayTag::RequestGameplayTag(TEXT("Structure.Action.Attack"))
+#define GASTAG_Player_Action_BuildStructure FGameplayTag::RequestGameplayTag(TEXT("Player.Action.BuildStructure"))
