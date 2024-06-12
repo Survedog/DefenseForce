4ec1f41f047f1e39346343e08245e378f5b0268f
survedog <survedog@gmail.com>
2024-06-12 22:27:23 +0900

    [FEAT]포물선 투사체를 날리는 타워를 다루는 DFArcProjectileTowerInterface 생성

---
+++ Source/DefenseForce/Interface/DFArcProjectileTowerInterface.h
@@ -0,0 +1,30 @@
+// Copyright 2024, Survedog. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "DFArcProjectileTowerInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI, BlueprintType, meta = (CannotImplementInterfaceInBlueprint))
+class UDFArcProjectileTowerInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class DEFENSEFORCE_API IDFArcProjectileTowerInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable)
+	virtual FVector GetProjectileSpawnLocation() const = 0;
+
+	UFUNCTION(BlueprintCallable)
+	virtual float GetProjectileLaunchSpeed() const = 0;
+};
