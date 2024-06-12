aab385d682fc5506de5e69917da0eeab6c821ed6
survedog <survedog@gmail.com>
2024-06-12 23:47:46 +0900

    [FEAT]DFArcProjectileTowerInterface: LaunchSpeed/Pitch를 가져오는 함수를 삭제하고 GetProjectileLaunchVelocity 함수 정의 [FIX]DFArcProjectileTowerInterface: 블루프린트에서 구현할 수 있도록 변경

--- Source/DefenseForce/Interface/DFArcProjectileTowerInterface.h
+++ Source/DefenseForce/Interface/DFArcProjectileTowerInterface.h
@@ -7,7 +7,7 @@
 #include "DFArcProjectileTowerInterface.generated.h"
 
 // This class does not need to be modified.
-UINTERFACE(MinimalAPI, BlueprintType, meta = (CannotImplementInterfaceInBlueprint))
+UINTERFACE(MinimalAPI, BlueprintType)
 class UDFArcProjectileTowerInterface : public UInterface
 {
 	GENERATED_BODY()
@@ -22,12 +22,9 @@ class DEFENSEFORCE_API IDFArcProjectileTowerInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-	UFUNCTION(BlueprintCallable)
-	virtual FVector GetProjectileSpawnLocation() const = 0;
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	FVector GetProjectileSpawnLocation() const;
 
-	UFUNCTION(BlueprintCallable)
-	virtual float GetProjectileLaunchSpeed() const = 0;
-
-	UFUNCTION(BlueprintCallable)
-	virtual float GetProjectileLaunchPitch() const = 0;
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	FVector GetProjectileLaunchVelocity() const;
 };
