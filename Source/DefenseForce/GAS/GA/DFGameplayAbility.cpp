c585698883c10f86551d506764f0d5f3beafbae6
survedog <survedog@gmail.com>
2024-06-18 15:48:35 +0900

    [FEAT]DFTowerBase: AttackCooldownTime 멤버 및 Getter 추가

---
+++ Source/DefenseForce/GAS/GA/DFGameplayAbility.cpp
@@ -0,0 +1,5 @@
+// Copyright 2024, Survedog. All rights reserved.
+
+
+#include "GAS/GA/DFGameplayAbility.h"
+
---
+++ Source/DefenseForce/GAS/GA/DFGameplayAbility.h
@@ -0,0 +1,17 @@
+// Copyright 2024, Survedog. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "DFGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEFENSEFORCE_API UDFGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
