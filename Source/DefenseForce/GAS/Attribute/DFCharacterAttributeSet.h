bcda2931fbc711e7cdf299db7d78735c9b5fdf6a
survedog <survedog@gmail.com>
2024-06-26 14:30:07 +0900

    [FIX]DFCharacterAttributeSet: Hp 관련 기능 삭제

--- Source/DefenseForce/GAS/Attribute/DFCharacterAttributeSet.h
+++ Source/DefenseForce/GAS/Attribute/DFCharacterAttributeSet.h
@@ -13,8 +13,6 @@
 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnHpZeroDelegate);
-
 /**
  * 
  */
@@ -26,45 +24,21 @@ class DEFENSEFORCE_API UDFCharacterAttributeSet : public UAttributeSet
 public:
 	UDFCharacterAttributeSet();
 
-	ATTRIBUTE_ACCESSORS(UDFCharacterAttributeSet, Hp);
-	ATTRIBUTE_ACCESSORS(UDFCharacterAttributeSet, MaxHp);
-	ATTRIBUTE_ACCESSORS(UDFCharacterAttributeSet, DamageToApply);
 	ATTRIBUTE_ACCESSORS(UDFCharacterAttributeSet, MoveSpeed);
-
-	UPROPERTY(BlueprintAssignable, Meta = (HideInDetailsView))
-	mutable FOnHpZeroDelegate OnHpZero;
 
 protected:
 	//virtual bool PreGameplayEffectExecute(struct FGameplayEffectModCallbackData& Data) override;
 	//virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
-	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
-	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+	//virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	//virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
 
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;	
 
 protected:
-	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Hp, Category = "Health", Meta = (AllowPrivateAccess = true))
-	FGameplayAttributeData Hp;
-
-	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHp, Category = "Health", Meta = (AllowPrivateAccess = true))
-	FGameplayAttributeData MaxHp;
-
-	UPROPERTY(BlueprintReadOnly, Category = "Health", Meta = (AllowPrivateAccess = true))
-	FGameplayAttributeData DamageToApply;
-
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MoveSpeed, Category = "Movement", Meta = (AllowPrivateAccess = true))
 	FGameplayAttributeData MoveSpeed;
 
 protected:
-	uint8 bIsDead : 1;
-
-protected:
-	UFUNCTION()
-	void OnRep_Hp(const FGameplayAttributeData& OldHp);
-
-	UFUNCTION()
-	void OnRep_MaxHp(const FGameplayAttributeData& OldMaxHp);
-
 	UFUNCTION()
 	void OnRep_MoveSpeed(const FGameplayAttributeData& OldMoveSpeed);
 };
