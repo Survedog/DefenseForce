8d9a4bd29bec026896b8cf6752ae79be7c081d1b
survedog <survedog@gmail.com>
2024-07-01 23:54:12 +0900

    [FEAT]적 캐릭터가 공격받을 시 DamageIndicator 위젯을 통해 대미지 양을 표시하도록 구현

---
+++ Source/DefenseForce/UI/DamageIndicatorWidget.cpp
@@ -0,0 +1,35 @@
+// Copyright 2024, Survedog. All rights reserved.
+
+
+#include "UI/DamageIndicatorWidget.h"
+#include "AbilitySystemComponent.h"
+#include "GAS/Attribute/DFHealthAttributeSet.h"
+
+UAbilitySystemComponent* UDamageIndicatorWidget::GetAbilitySystemComponent() const
+{
+	return ASC.Get();
+}
+
+void UDamageIndicatorWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (ASC.IsValid())
+	{
+		ASC->GetGameplayAttributeValueChangeDelegate(UDFHealthAttributeSet::GetHpAttribute()).AddUObject(this, &UDamageIndicatorWidget::OnOwnerHpChangedCallback);
+	}
+}
+
+void UDamageIndicatorWidget::OnOwnerHpChangedCallback(const FOnAttributeChangeData& AttributeChangeData)
+{
+	const float DamageAmount = AttributeChangeData.OldValue - AttributeChangeData.NewValue;
+	if (DamageAmount >= 0.0f)
+	{
+		ShowDamageAmount(DamageAmount);
+	}
+}
+
+void UDamageIndicatorWidget::SetAbilitySystemComponent(UAbilitySystemComponent* NewASC)
+{
+	ASC = NewASC;
+}
