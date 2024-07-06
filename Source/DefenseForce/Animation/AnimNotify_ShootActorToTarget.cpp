b37d980cb1209fc2f2f2df1eb0d6cb20d2a1cb07
survedog <survedog@gmail.com>
2024-07-06 23:59:48 +0900

    [FEAT]AnimNotify_ShootActorToTarget 생성 및 구현

---
+++ Source/DefenseForce/Animation/AnimNotify_ShootActorToTarget.cpp
@@ -0,0 +1,37 @@
+// Copyright 2024, Survedog. All rights reserved.
+
+
+#include "Animation/AnimNotify_ShootActorToTarget.h"
+#include "Interface/DFProjectileTowerInterface.h"
+#include "Projectile/DFProjectileBase.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+UAnimNotify_ShootActorToTarget::UAnimNotify_ShootActorToTarget()
+{
+	bShouldFireInEditor = false;
+}
+
+FString UAnimNotify_ShootActorToTarget::GetNotifyName_Implementation() const
+{
+	return TEXT("ShootActorToTarget");
+}
+
+void UAnimNotify_ShootActorToTarget::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	IDFProjectileTowerInterface* ProjectileThrowerInterface = Cast<IDFProjectileTowerInterface>(MeshComp->GetOwner());
+	if (ProjectileThrowerInterface)
+	{
+		const TSubclassOf<AActor> ProjectileClass = ProjectileThrowerInterface->GetProjectileActorClass();
+		const FVector SpawnLocation = ProjectileThrowerInterface->GetProjectileSpawnLocation();
+
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.Owner = MeshComp->GetOwner();
+		SpawnParams.Instigator = Cast<APawn>(MeshComp->GetOwner());
+		
+		ADFProjectileBase* SpawnedProjectile = GetWorld()->SpawnActor<ADFProjectileBase>(ProjectileClass, SpawnParams);
+		UProjectileMovementComponent* ProjectileMovementComp = SpawnedProjectile->GetComponentByClass<UProjectileMovementComponent>();
+		ProjectileMovementComp->Velocity = ProjectileThrowerInterface->GetProjectileLaunchVelocity();
+	}
+}
